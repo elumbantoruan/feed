2291d3e6a59eb933c3dc3842bc32733a9dda291e
Edison <edison@k8smaster.edison.net>
2024-01-07 21:09:22 -0800

    update cronjob yaml

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,12 +3,16 @@ package config
 import "os"
 
 type Config struct {
-	DBConn string
+	DBConn         string
+	DiscordWebhook string
 }
 
 func NewConfig() Config {
 	conn := os.Getenv("DB_CONN")
+	discord := os.Getenv("DISCORD_WEBHOOK")
+
 	return Config{
-		DBConn: conn,
+		DBConn:         conn,
+		DiscordWebhook: discord,
 	}
 }
